79c39dfcec0f6a68db209f0fc1be27d51c8a6840
Luaranet <luaranet08@gmail.com>
2023-05-17 14:28:59 -0400

    comit inicial

--- script.js
+++ script.js
@@ -16,16 +16,20 @@ Mostrar(){
 
 
 class ProdutoDestaque extends produto{
-constructor(nome, datadecadastro, descricao, preco, img){
-super(nome, datadecadastro, descricao , preco)
-this.img = img;
-}
-Mostrar_destaque(){
-    return `<div>${this.nome}</div>`;
-    return this.nome + this.datadecadastro + this.descricao + this.preco + this.img
+    constructor(nome, datadecadastro, descricao, preco, img){
+        super(nome, datadecadastro, descricao, preco);
+        this.img = img;
+    }
+
+    Mostrar_destaque(){
+        return `<center><img src="${this.img}"></center> 
+    <center><h2>${this.nome}</h2></center> 
+    <center><h5>Descrição:  ${this.descricao}</h5></center> 
+    <center><h5>Data de Cadastro:  ${this.datadecadastro}</h5></center> <center><h5>Preço:  ${this.preco}</h5></center>`
+
+    }
 }
 
-}
 let Produto = new produto("Colar","27/05/2017","complementar a roupa", 35)
 console.log(produto.Mostrar())
  
