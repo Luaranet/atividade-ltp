cc6474719ce665481a6975e496d6735bc1a13536
Luaranet <luaranet08@gmail.com>
2023-05-17 13:34:11 -0400

    comit inicial

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ class Produto {
 }
 
 Mostrar(){
+    return `<div>${this.nome}</div>`;
  return this.nome + this.datadecadastro + this.descricao + this.preco
 }
 
@@ -15,13 +16,13 @@ Mostrar(){
 
 
 class ProdutoDestaque extends produto{
-constructor(nome,datadecadastro, descricao ,preco , img){
-super(nome,datadecadastro, descricao ,preco)
+constructor(nome, datadecadastro, descricao, preco, img){
+super(nome, datadecadastro, descricao , preco)
 this.img = img;
 }
 Mostrar_destaque(){
+    return `<div>${this.nome}</div>`;
     return this.nome + this.datadecadastro + this.descricao + this.preco + this.img
-
 }
 
 }
@@ -30,6 +31,7 @@ console.log(produto.Mostrar())
  
 let ProdutoDestaque = new produtoDestaque("Anel de pedra","28/06/2017","Embelazar as mãos", 15,"https://img.ltwebstatic.com/images3_pi/2021/08/03/1627956390965f0ccbfef1d6b8abc816c5652a68d6_thumbnail_900x.webp")
 console.log(ProdutoDestaque.Mostrar_destaque())
+
 
 
 
